8a9cefe3a2e6a7a18efc48c5cadfb9833123dabf
Sebastian C. Sabry <sebastiancsabry@system76-pc.localdomain>
2024-01-08 22:56:36 -0800

    Implemented light, specular light, shadows, spheres

--- build/CMakeFiles/tinyraytracer.dir/tinyraytracer.cpp.o.d
+++ build/CMakeFiles/tinyraytracer.dir/tinyraytracer.cpp.o.d
@@ -1,6 +1,8 @@
 CMakeFiles/tinyraytracer.dir/tinyraytracer.cpp.o: \
  /home/sebastiancsabry/Documents/Projects/rayTracingBasic/tinyraytracer/tinyraytracer.cpp \
- /usr/include/stdc-predef.h /usr/include/c++/11/limits \
+ /usr/include/stdc-predef.h \
+ /home/sebastiancsabry/Documents/Projects/rayTracingBasic/tinyraytracer/geometry.h \
+ /usr/include/c++/11/cmath \
  /usr/include/x86_64-linux-gnu/c++/11/bits/c++config.h \
  /usr/include/x86_64-linux-gnu/c++/11/bits/os_defines.h \
  /usr/include/features.h /usr/include/features-time64.h \
@@ -11,7 +13,7 @@ CMakeFiles/tinyraytracer.dir/tinyraytracer.cpp.o: \
  /usr/include/x86_64-linux-gnu/gnu/stubs.h \
  /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
  /usr/include/x86_64-linux-gnu/c++/11/bits/cpu_defines.h \
- /usr/include/c++/11/cmath /usr/include/c++/11/bits/cpp_type_traits.h \
+ /usr/include/c++/11/bits/cpp_type_traits.h \
  /usr/include/c++/11/ext/type_traits.h /usr/include/math.h \
  /usr/include/x86_64-linux-gnu/bits/libc-header-start.h \
  /usr/include/x86_64-linux-gnu/bits/types.h \
@@ -59,12 +61,41 @@ CMakeFiles/tinyraytracer.dir/tinyraytracer.cpp.o: \
  /usr/include/x86_64-linux-gnu/bits/struct_rwlock.h /usr/include/alloca.h \
  /usr/include/x86_64-linux-gnu/bits/stdlib-bsearch.h \
  /usr/include/x86_64-linux-gnu/bits/stdlib-float.h \
- /usr/include/x86_64-linux-gnu/bits/stdlib.h /usr/include/c++/11/iostream \
+ /usr/include/x86_64-linux-gnu/bits/stdlib.h /usr/include/c++/11/vector \
+ /usr/include/c++/11/bits/stl_algobase.h \
+ /usr/include/c++/11/bits/functexcept.h \
+ /usr/include/c++/11/bits/exception_defines.h \
+ /usr/include/c++/11/ext/numeric_traits.h \
+ /usr/include/c++/11/bits/stl_pair.h /usr/include/c++/11/bits/move.h \
+ /usr/include/c++/11/type_traits \
+ /usr/include/c++/11/bits/stl_iterator_base_types.h \
+ /usr/include/c++/11/bits/stl_iterator_base_funcs.h \
+ /usr/include/c++/11/bits/concept_check.h \
+ /usr/include/c++/11/debug/assertions.h \
+ /usr/include/c++/11/bits/stl_iterator.h \
+ /usr/include/c++/11/bits/ptr_traits.h /usr/include/c++/11/debug/debug.h \
+ /usr/include/c++/11/bits/predefined_ops.h \
+ /usr/include/c++/11/bits/allocator.h \
+ /usr/include/x86_64-linux-gnu/c++/11/bits/c++allocator.h \
+ /usr/include/c++/11/ext/new_allocator.h /usr/include/c++/11/new \
+ /usr/include/c++/11/bits/exception.h \
+ /usr/include/c++/11/bits/memoryfwd.h \
+ /usr/include/c++/11/bits/stl_construct.h \
+ /usr/include/c++/11/bits/stl_uninitialized.h \
+ /usr/include/c++/11/ext/alloc_traits.h \
+ /usr/include/c++/11/bits/alloc_traits.h \
+ /usr/include/c++/11/bits/stl_vector.h \
+ /usr/include/c++/11/initializer_list \
+ /usr/include/c++/11/bits/stl_bvector.h \
+ /usr/include/c++/11/bits/functional_hash.h \
+ /usr/include/c++/11/bits/hash_bytes.h \
+ /usr/include/c++/11/bits/range_access.h \
+ /usr/include/c++/11/bits/vector.tcc /usr/include/c++/11/cassert \
+ /usr/include/assert.h /usr/include/c++/11/iostream \
  /usr/include/c++/11/ostream /usr/include/c++/11/ios \
  /usr/include/c++/11/iosfwd /usr/include/c++/11/bits/stringfwd.h \
- /usr/include/c++/11/bits/memoryfwd.h /usr/include/c++/11/bits/postypes.h \
- /usr/include/c++/11/cwchar /usr/include/wchar.h \
- /usr/lib/gcc/x86_64-linux-gnu/11/include/stdarg.h \
+ /usr/include/c++/11/bits/postypes.h /usr/include/c++/11/cwchar \
+ /usr/include/wchar.h /usr/lib/gcc/x86_64-linux-gnu/11/include/stdarg.h \
  /usr/include/x86_64-linux-gnu/bits/wchar.h \
  /usr/include/x86_64-linux-gnu/bits/types/wint_t.h \
  /usr/include/x86_64-linux-gnu/bits/types/mbstate_t.h \
@@ -72,26 +103,10 @@ CMakeFiles/tinyraytracer.dir/tinyraytracer.cpp.o: \
  /usr/include/x86_64-linux-gnu/bits/types/__FILE.h \
  /usr/include/x86_64-linux-gnu/bits/types/FILE.h \
  /usr/include/x86_64-linux-gnu/bits/wchar2.h \
- /usr/include/c++/11/exception /usr/include/c++/11/bits/exception.h \
- /usr/include/c++/11/bits/exception_ptr.h \
- /usr/include/c++/11/bits/exception_defines.h \
+ /usr/include/c++/11/exception /usr/include/c++/11/bits/exception_ptr.h \
  /usr/include/c++/11/bits/cxxabi_init_exception.h \
- /usr/include/c++/11/typeinfo /usr/include/c++/11/bits/hash_bytes.h \
- /usr/include/c++/11/new /usr/include/c++/11/bits/move.h \
- /usr/include/c++/11/type_traits \
- /usr/include/c++/11/bits/nested_exception.h \
- /usr/include/c++/11/bits/char_traits.h \
- /usr/include/c++/11/bits/stl_algobase.h \
- /usr/include/c++/11/bits/functexcept.h \
- /usr/include/c++/11/ext/numeric_traits.h \
- /usr/include/c++/11/bits/stl_pair.h \
- /usr/include/c++/11/bits/stl_iterator_base_types.h \
- /usr/include/c++/11/bits/stl_iterator_base_funcs.h \
- /usr/include/c++/11/bits/concept_check.h \
- /usr/include/c++/11/debug/assertions.h \
- /usr/include/c++/11/bits/stl_iterator.h \
- /usr/include/c++/11/bits/ptr_traits.h /usr/include/c++/11/debug/debug.h \
- /usr/include/c++/11/bits/predefined_ops.h /usr/include/c++/11/cstdint \
+ /usr/include/c++/11/typeinfo /usr/include/c++/11/bits/nested_exception.h \
+ /usr/include/c++/11/bits/char_traits.h /usr/include/c++/11/cstdint \
  /usr/lib/gcc/x86_64-linux-gnu/11/include/stdint.h /usr/include/stdint.h \
  /usr/include/x86_64-linux-gnu/bits/stdint-uintn.h \
  /usr/include/c++/11/bits/localefwd.h \
@@ -116,19 +131,11 @@ CMakeFiles/tinyraytracer.dir/tinyraytracer.cpp.o: \
  /usr/include/x86_64-linux-gnu/c++/11/bits/atomic_word.h \
  /usr/include/x86_64-linux-gnu/sys/single_threaded.h \
  /usr/include/c++/11/bits/locale_classes.h /usr/include/c++/11/string \
- /usr/include/c++/11/bits/allocator.h \
- /usr/include/x86_64-linux-gnu/c++/11/bits/c++allocator.h \
- /usr/include/c++/11/ext/new_allocator.h \
  /usr/include/c++/11/bits/ostream_insert.h \
  /usr/include/c++/11/bits/cxxabi_forced.h \
  /usr/include/c++/11/bits/stl_function.h \
  /usr/include/c++/11/backward/binders.h \
- /usr/include/c++/11/bits/range_access.h \
- /usr/include/c++/11/initializer_list \
  /usr/include/c++/11/bits/basic_string.h \
- /usr/include/c++/11/ext/alloc_traits.h \
- /usr/include/c++/11/bits/alloc_traits.h \
- /usr/include/c++/11/bits/stl_construct.h \
  /usr/include/c++/11/ext/string_conversions.h /usr/include/c++/11/cstdlib \
  /usr/include/c++/11/cstdio /usr/include/stdio.h \
  /usr/include/x86_64-linux-gnu/bits/types/__fpos_t.h \
@@ -143,7 +150,6 @@ CMakeFiles/tinyraytracer.dir/tinyraytracer.cpp.o: \
  /usr/include/asm-generic/errno.h /usr/include/asm-generic/errno-base.h \
  /usr/include/x86_64-linux-gnu/bits/types/error_t.h \
  /usr/include/c++/11/bits/charconv.h \
- /usr/include/c++/11/bits/functional_hash.h \
  /usr/include/c++/11/bits/basic_string.tcc \
  /usr/include/c++/11/bits/locale_classes.tcc \
  /usr/include/c++/11/system_error \
@@ -159,14 +165,14 @@ CMakeFiles/tinyraytracer.dir/tinyraytracer.cpp.o: \
  /usr/include/c++/11/bits/locale_facets.tcc \
  /usr/include/c++/11/bits/basic_ios.tcc \
  /usr/include/c++/11/bits/ostream.tcc /usr/include/c++/11/istream \
- /usr/include/c++/11/bits/istream.tcc /usr/include/c++/11/fstream \
+ /usr/include/c++/11/bits/istream.tcc /usr/include/c++/11/algorithm \
+ /usr/include/c++/11/utility /usr/include/c++/11/bits/stl_relops.h \
+ /usr/include/c++/11/bits/stl_algo.h \
+ /usr/include/c++/11/bits/algorithmfwd.h \
+ /usr/include/c++/11/bits/stl_heap.h \
+ /usr/include/c++/11/bits/stl_tempbuf.h \
+ /usr/include/c++/11/bits/uniform_int_dist.h /usr/include/c++/11/fstream \
  /usr/include/c++/11/bits/codecvt.h \
  /usr/include/x86_64-linux-gnu/c++/11/bits/basic_file.h \
  /usr/include/x86_64-linux-gnu/c++/11/bits/c++io.h \
- /usr/include/c++/11/bits/fstream.tcc /usr/include/c++/11/vector \
- /usr/include/c++/11/bits/stl_uninitialized.h \
- /usr/include/c++/11/bits/stl_vector.h \
- /usr/include/c++/11/bits/stl_bvector.h \
- /usr/include/c++/11/bits/vector.tcc \
- /home/sebastiancsabry/Documents/Projects/rayTracingBasic/tinyraytracer/geometry.h \
- /usr/include/c++/11/cassert /usr/include/assert.h
+ /usr/include/c++/11/bits/fstream.tcc /usr/include/c++/11/limits
--- tinyraytracer.cpp
+++ tinyraytracer.cpp
@@ -1,35 +1,165 @@
-#include <limits>
+#include "geometry.h" // NOTICE: the class of vectors lives inside the geometry.h file
+#include <algorithm>
 #include <cmath>
-#include <iostream>
+#include <cstdio>
 #include <fstream>
-#include <vector> // NOTICE: Use of cpp vector 
-#include "geometry.h"// NOTICE: the class of vectors lives inside the geometry.h file
+#include <iostream>
+#include <limits>
+#include <vector> // NOTICE: Use of cpp vector
 
-void render() {
-    const int width    = 1024;
-    const int height   = 768;
-    std::vector<Vec3f> framebuffer(width*height);
+// Create the material struct
+struct Material {
+  Material(const Vec2f &a, const Vec3f &color, const float &spec)
+      : albedo(a), diffuse_color(color), specular_exponent(spec) {}
+  Material() : albedo(1, 0), diffuse_color(), specular_exponent() {}
+  Vec2f albedo;
+  Vec3f diffuse_color;
+  float specular_exponent;
+};
 
-    for (size_t j = 0; j<height; j++) {
-        for (size_t i = 0; i<width; i++) {
-            framebuffer[i+j*width] = Vec3f(j/float(height),i/float(width), 0);
-        }
+// Create the light struct
+//
+struct Light {
+  Light(const Vec3f &p, const float &i) : position(p), intensity(i) {}
+  Vec3f position;
+  float intensity;
+};
+
+// Create a Sphere struct which implements functions to understand ray
+// intersections We need only four numbers to describe a sphere in memeory a
+// three-dimensional vector for the centre and a scale for the radius
+//
+//
+struct Sphere {
+  Vec3f center;
+  float radius;
+  Material material;
+
+  Sphere(const Vec3f &c, const float &r, const Material &m)
+      : center(c), radius(r), material(m) {}
+
+  bool ray_intersect(const Vec3f &orig, const Vec3f &dir, float &t0) {
+    // This function is based of the Ray-Sphere Intersection algorithm
+    //
+    Vec3f L = center - orig;
+    float tca = L * dir;
+    float d2 = L * L - tca * tca;
+    if (d2 > radius * radius)
+      return false;
+    float thc = sqrtf(radius * radius - d2);
+    t0 = tca - thc;
+    float t1 = tca + thc;
+    if (t0 < 0)
+      t0 = t1;
+    if (t0 < 0)
+      return false;
+    return true;
+  }
+};
+
+Vec3f reflect(const Vec3f &I, const Vec3f &N) { return I - N * 2.f * (I * N); }
+
+bool scene_interesect(const Vec3f &orig, const Vec3f &dir,
+                      std::vector<Sphere> &spheres, Vec3f &hit, Vec3f &N,
+                      Material &material) {
+  float spheres_dist = std::numeric_limits<float>::max();
+  for (size_t i = 0; i < spheres.size(); i++) {
+    float dist_i;
+    if (spheres[i].ray_intersect(orig, dir, dist_i) && dist_i < spheres_dist) {
+      spheres_dist = dist_i;
+      hit = orig + dir * dist_i;
+      N = (hit - spheres[i].center).normalize();
+      material = spheres[i].material;
     }
+  }
+  return spheres_dist < 1000;
+}
 
-    std::ofstream ofs; // save the framebuffer to file
-    ofs.open("./out.ppm");
-    ofs << "P6\n" << width << " " << height << "\n255\n";
-    for (size_t i = 0; i < height*width; ++i) {
-        for (size_t j = 0; j<3; j++) {
-            ofs << (char)(255 * std::max(0.f, std::min(1.f, framebuffer[i][j])));
-        }
+Vec3f cast_ray(const Vec3f &orig, const Vec3f &dir,
+               std::vector<Sphere> &spheres, const std::vector<Light> &lights) {
+  Vec3f point, N;
+  Material material;
+
+  if (!scene_interesect(orig, dir, spheres, point, N, material)) {
+    return Vec3f(0.2, 0.7, 0.8); // Background color
+  }
+
+  float diffuse_light_intensity = 0, specular_light_intensity = 0;
+  for (size_t i = 0; i < lights.size(); i++) {
+    Vec3f light_dir = (lights[i].position - point).normalize();
+    //SHADOWS:
+    float light_distance = (lights[i].position - point).norm(); 
+    Vec3f shadow_orig = light_dir*N < 0 ? point - N*1e-3 : point + N*1e-3; //Checking if the point lies in the shadow of the lights[i]
+    Vec3f shadow_pt, shadow_N; 
+    Material tmp_material; 
+    if (scene_interesect(shadow_orig, light_dir, spheres, shadow_pt, shadow_N, tmp_material) && (shadow_pt - shadow_orig).norm() < light_distance) continue; //In other words, we don't render the diffusion and specular light if this is the case
+    //diffuse light 
+    diffuse_light_intensity +=
+        lights[i].intensity * std::max(0.f, light_dir * N);
+    //Speculatar light
+    specular_light_intensity +=
+        pow(std::max(0.f, -reflect(-light_dir, N) * dir),
+            material.specular_exponent) *
+        lights[i].intensity;
+  }
+
+  return material.diffuse_color * diffuse_light_intensity * material.albedo[0] + Vec3f(1., 1., 1.)*specular_light_intensity*material.albedo[1];
+}
+
+void render(std::vector<Sphere> &spheres, const std::vector<Light> &lights) {
+  const int width = 1024;
+  const int height = 768;
+  const float fov = M_PI / 2;
+  std::vector<Vec3f> framebuffer(width * height);
+
+  for (size_t j = 0; j < height; j++) {
+    for (size_t i = 0; i < width; i++) {
+      float x = (2 * (i + 0.5) / (float)width - 1) * tan(fov / 2.) * width /
+                (float)height;
+      (float)height;
+      float y = -(2 * (j + 0.5) / (float)height - 1) * tan(fov / 2.);
+      Vec3f dir = Vec3f(x, y, -1).normalize();
+      framebuffer[i + j * width] =
+          cast_ray(Vec3f(0, 0, 0), dir, spheres, lights);
     }
-    ofs.close();
+  }
+
+  std::ofstream ofs; // save the framebuffer to file
+  ofs.open("./out.ppm");
+  ofs << "P6\n" << width << " " << height << "\n255\n";
+  for (size_t i = 0; i < height * width; ++i) {
+    for (size_t j = 0; j < 3; j++) {
+      Vec3f &c = framebuffer[i];
+      float max = std::max(c[0], std::max(c[1], c[2]));
+      if (max > 1)
+        c = c * (1. / max);
+      ofs << (char)(255 * std::max(0.f, std::min(1.f, framebuffer[i][j])));
+    }
+  }
+  ofs.close();
 }
 
 int main() {
-    render();
+  Material ivory(Vec2f(0.6, 0.3), Vec3f(0.4, 0.4, 0.3), 50.);
+  Material red_rubber(Vec2f(0.9, 0.1), Vec3f(0.3, 0.1, 0.1), 10.);
 
-    return 0;
+  // Create our example spheres
+  std::vector<Sphere> spheres;
+  spheres.push_back(Sphere(Vec3f(-3, 0, -16), 2, ivory));
+  spheres.push_back(Sphere(Vec3f(-1.0, -1.5, -12), 2, red_rubber));
+  spheres.push_back(Sphere(Vec3f(1.5, -0.5, -18), 3, red_rubber));
+  spheres.push_back(Sphere(Vec3f(7, 5, -18), 4, ivory));
+
+  // Create the light
+  std::vector<Light> lights;
+  lights.push_back(Light(Vec3f(-20, 20, 20), 1.5));
+  lights.push_back(Light(Vec3f(30, 50, -25), 1.8));
+  lights.push_back(Light(Vec3f(30, 20, 30), 1.7));
+
+  // Rendering:
+  render(spheres, lights);
+
+  std::cout << "Rendering finished.\n";
+
+  return 0;
 }
-
